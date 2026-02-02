4360eb3d26c90b1d949ae438eb8837d7c4db1180
eshwar-iiti <me240003029@iiti.ac.in>
2026-02-02 20:06:45 +0530

    Add transaction.h with Input, Output, and Transaction structs
    Define structures for transaction inputs, outputs, and transactions.

---
+++ src/transaction.h
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include"utxo_manager.h"
+using namespace std;
+
+//input format
+struct Input 
+{
+    string prev_tx_id;
+    int index;
+    string owner; 
+};
+
+//output format
+struct Output 
+{
+    double amount;
+    string address;
+};
+
+struct Transaction 
+{
+    string tx_id;
+    vector<Input> inputs;
+    vector<Output> outputs;
+    double fee;
+
+};
